3f8586636adaf2b234c8e06a9e55b2f70fb06993
Michael King <mtking1123@gmail.com>
2018-08-28 12:54:29 -0400

    allow stopping of thread

--- Philosopher.py
+++ Philosopher.py
@@ -4,22 +4,32 @@ class Philosopher(threading.Thread):
 
   def __init__(self, name, left_chopstick, right_chopstick, color):
     threading.Thread.__init__(self)
+    self.stopped = False
     self.name = color + name + '\033[00m'
     self.left_chopstick = left_chopstick
     self.right_chopstick = right_chopstick
 
-  def run(self):
-
-    while True:
-      print(f'{self.name} is waiting.')
+  def pickup(self):
+    if not self.stopped:
       self.left_chopstick.acquire()
       self.right_chopstick.acquire()
 
+  def putdown(self):
+    self.left_chopstick.release()
+    self.right_chopstick.release()
+
+  def stop(self):
+    self.stopped = True
+
+  def run(self):
+    while not self.stopped:
+      print(f'{self.name} is waiting.')
+      self.pickup()
+      if self.stopped: return
+
       print(f'{self.name} is eating.')
       time.sleep(random.randint(3, 10))
-
-      self.left_chopstick.release()
-      self.right_chopstick.release()
+      self.putdown()
 
       print(f'{self.name} is thinking.')
-      time.sleep(random.randint(3, 15))
+      if not self.stopped: time.sleep(random.randint(3, 15))
